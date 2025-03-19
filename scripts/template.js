9c497267b709bd2ed29e5dfd7bc0bbb6910915f3
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-03-19 17:43:54 +0100

    added button

--- scripts/template.js
+++ scripts/template.js
@@ -2,11 +2,14 @@ function dishesGallery(i){
 
     return /*html*/`
     <div id="dish${i + 1}" class="dish">
+        <button id="addMeal${i + 1}" class="addMeal">Add</button>
+        <div id="Properties${i + 1}" class="Properties">
+
         <div id="dishName${i + 1}" class="dishName">${meals[i].name}</div>
-        <div id="dishName${i + 1}" class="dishName">${meals[i].price.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})}</div>
-        <div id="dishName${i + 1}" class="dishName">${meals[i].description}</div>
+        <div id="dishPrice${i + 1}" class="dishName">${meals[i].price.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})}</div>
+        <div id="dishDescription${i + 1}" class="dishName">${meals[i].description}</div>
 
-
+        </div>
         </div>
     `
 }
