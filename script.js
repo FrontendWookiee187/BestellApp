83e52765ee9821bfa46d3239629663012eeaa9cf
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-03-26 15:10:25 +0100

    added some functions and style

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
-function init() {
-  renderDishesGallery();
-  renderTotal(cartItems);
-}
-
-function renderDishesGallery() {
-  for (let i = 0; i < meals.length; i++) {
-    document.getElementById("dishes-container").innerHTML += dishesGallery(i);
+  function init() {
+   renderDishesGallery();
+   renderTotal(cartItems);
   }
-}
 
-function addMealtoCart(i) {
-  document.getElementById("orderedMeals").innerHTML = ""
-  let addMealNameValue = meals[i].name;
-  let addMealPriceValue = meals[i].price;
-  let addMeal = { name: addMealNameValue, price: addMealPriceValue, amount: 1 };
-  let newItem = true;
-  for (let i = 0; i < cartItems.length; i++) {
+  function renderDishesGallery() {
+    for (let i = 0; i < meals.length; i++) {
+    document.getElementById("dishes-container").innerHTML += dishesGallery(i);
+   }
+  }
+
+  function addMealtoCart(i) {
+   document.getElementById("orderedMeals").innerHTML = ""
+   let addMealNameValue = meals[i].name;
+   let addMealPriceValue = meals[i].price;
+   let addMeal = { name: addMealNameValue, price: addMealPriceValue, amount: 1 };
+   let newItem = true;
+   for (let i = 0; i < cartItems.length; i++) {
     if (cartItems[i].name === addMeal.name) {
       cartItems[i].amount = cartItems[i].amount + 1;
       cartItems[i].price = addMeal.price * cartItems[i].amount;
       newItem = false;
-    }
+     }
+   }
+   if (newItem === true) {
+     cartItems.push(addMeal);
+   }
+    document.getElementById("orderedMeals").innerHTML = orderedMealsTemplate(cartItems);
+   renderTotal(cartItems);
   }
-  if (newItem === true) {
-    cartItems.push(addMeal);
+
+  function increase(i) {
+   cartItems[i].amount = cartItems[i].amount + 1;
+    cartItems[i].price = meals[i].price * cartItems[i].amount;
+   document.getElementById("orderedMeals").innerHTML = orderedMealsTemplate(cartItems);
+   renderTotal(cartItems);
   }
-  document.getElementById("orderedMeals").innerHTML = orderedMealsTemplate(cartItems);
-  renderTotal(cartItems);
-}
 
-function increase(i) {
-  cartItems[i].amount = cartItems[i].amount + 1;
-  cartItems[i].price = meals[i].price * cartItems[i].amount;
-  document.getElementById("orderedMeals").innerHTML = orderedMealsTemplate(cartItems);
-  renderTotal(cartItems);
-}
-
-function decrease(i) {
+  function decrease(i) {
   if (cartItems[i].amount <= 1) {
   } else {
     cartItems[i].amount = cartItems[i].amount - 1;
     cartItems[i].price = meals[i].price * cartItems[i].amount;
-
     document.getElementById("orderedMeals").innerHTML = orderedMealsTemplate(cartItems);
     renderTotal(cartItems);
+   }
   }
-}
 
-function deleteItem(i) {
-  cartItems.splice(i, 1);
-  document.getElementById("orderedMeals").innerHTML =
-  orderedMealsTemplate(cartItems);
-  renderTotal(cartItems);
-}
+  function deleteItem(i) {
+   cartItems.splice(i, 1);
+   document.getElementById("orderedMeals").innerHTML =
+   orderedMealsTemplate(cartItems);
+   renderTotal(cartItems);
+  }
 
-function OverlayOn() {
-  document.getElementById("overlay").style.display = "block";
-}
+  function OverlayOn() {
+    document.getElementById("overlay").style.display = "block";
+  }
 
-function OverlayOff() {
-  document.getElementById("overlay").style.display = "none";
-} 
+  function OverlayOff() {
+   document.getElementById("overlay").style.display = "none";
+  } 
 
   function mealsInResponsiveCart(cartItems){
     let cartContentNormal = document.getElementById('order');
@@ -69,8 +68,7 @@ function OverlayOff() {
   }
 
   function renderFromResponsiveCartToCart(cartItems){
-    document.getElementById("orderedMealsResponsive").innerHTML = ""
-    
+    document.getElementById("orderedMealsResponsive").innerHTML = ""    
     document.getElementById("orderedMeals").innerHTML = orderedMealsTemplate(cartItems);
     renderTotal(cartItems);
   }
@@ -83,10 +81,27 @@ function OverlayOff() {
     else{
     document.getElementById("orderedMeals").innerHTML = ""
     document.getElementById("orderedMeals").innerHTML += confirmOrderRenderResponse()
-    cartItems.splice(i, 1);
+    cartItems.splice(0);
     
     document.getElementById("totalSum").innerHTML = "Gesamt: " + totalPriceAfterOrder.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'});  
     document.getElementById("orderBTN").innerHTML = "Gesamt: " + totalPriceAfterOrder.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'});     
+    }
+    setTimeout(showEmptyBasket, 3000)
   }
+
+  function renderTotal(cartItems){
+    let totalPrice = 0;
+
+    for (let i = 0; i < cartItems.length; i++) {    
+    totalPrice += cartItems[i].price;    
+    }
+
+    document.getElementById("totalSum").innerHTML = "Gesamt: " + totalPrice.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'});
+    document.getElementById("responsiveBtn").innerHTML = "Warenkorb (" + totalPrice.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})+ ")";
+    document.getElementById("orderBTN").innerHTML = "Zahlungspflichtig bestellen (" + totalPrice.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})+ ")";
+
+    if (cartItems.length === 0){
+      showEmptyBasket()
+  } else{}
   }
 
--- scripts/template.js
+++ scripts/template.js
@@ -4,25 +4,21 @@ function dishesGallery(i){
     <div id="dish${i + 1}" class="dish">
         <button id="addMeal${i}" class="addMeal" onclick="addMealtoCart(${i})" ><img src="assets/img/plus_icon_circle_transparent.png" alt=""></button>
         <div id="Properties${i}" class="Properties">
-
         <div id="dishName${i}" class="dishName"><h3>${meals[i].name}</h3></div>
         <div id="dishPrice${i}" class="dishName">${meals[i].price.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})}</div>
         <div id="dishDescription${i}" class="dishName">${meals[i].description}</div>
-
         </div>
-        </div>
+    </div>
     `
 }
 
-function orderedMealsTemplate(cartItems){
-
-    let cart = ""
+function orderedMealsTemplate(cartItems){  
+let cart = ""
 
     for (let i = 0; i < cartItems.length; i++) {
 
         cart +=  /*html*/`
         <div id="meal${i}" class="meal">
-
             <button id="decrease${i}" class="inDecrease" onclick="decrease(${i})"><img src="assets/img/minus_icon_transparent.png" alt=""></button>   
             <div id="amountCounter${i}" class="amountCounter">${cartItems[i].amount}x</div>
             <button id="increase${i}" class="inDecrease" onclick="increase(${i})"><img src="assets/img/plus_icon_transparent.png" alt=""></button>
@@ -31,35 +27,33 @@ function orderedMealsTemplate(cartItems){
                 <div id="orderedMealPrice${i}" class="orderedMeal">${cartItems[i].price.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})}</div>
             </div>
             <button id="delteDish${i}" onclick="(deleteItem(${i}))" class="deleteBTN"><img src="assets/img/delete_forever_.png" alt=""></button>    
-            </div>     `
+        </div>     `
             }
-    return cart
-}
-
-function renderTotal(cartItems){
-
-    let totalPrice = 0;
-
-    for (let i = 0; i < cartItems.length; i++) {
-    
-        totalPrice += cartItems[i].price;
-    
-}
-
-document.getElementById("totalSum").innerHTML = "Gesamt: " + totalPrice.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'});
-document.getElementById("responsiveBtn").innerHTML = "Warenkorb (" + totalPrice.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})+ ")";
-document.getElementById("orderBTN").innerHTML = "Zahlungspflichtig bestellen (" + totalPrice.toLocaleString("de-DE", {style: 'currency', currency: 'EUR'})+ ")";
+        return cart    
 }
 
 function confirmOrderRenderResponse(){
-
     return /*html*/`
     <div class="orderConfirmationContainer" id="orderConfirmationContainer">
         <div class="orderConfirmation" id="orderConfirmation">Vielen Dank für Ihre Bestellung. Sie werden zeitnah gesättigt!!!</div>
-        <div class="confirmationSmiley" id="confirmationSmiley"> <img src="assets/img/smiley_icon_transparent.png" alt=""></div>
-  
+        <div class="confirmationSmiley" id="confirmationSmiley"> <img src="assets/img/smiley_icon_transparent.png" alt=""></div>  
   </div>
   `
+}
+
+function showEmptyBasket(){
+    document.getElementById("orderedMeals").innerHTML = ""
+    document.getElementById("orderedMeals").innerHTML = 
+
+    /*html*/`         <div class="emptyCartContainer" id="emptyCartContainer">
+                <div class="emptyCart" id="emptyCart">
+                  <img src="assets/img/cart-963.png" alt="" />
+                  <div class="emptyText" id="emptyText">
+                    Gäääähnende Leere hier...
+                  </div>
+                </div>
+              </div>
+    `
 }
 
 
